0b50185a856494390fff966cd8633cb32db1cfca
Sándor Czettner <sandor@czettner.hu>
2016-07-27 19:34:49 +0100

    One to ten

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -5,9 +5,22 @@ import android.os.Bundle;
 
 public class NumbersActivity extends AppCompatActivity {
 
+    public String [] words = new String[10];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        words[0] = "one";
+        words[1] = "two";
+        words[2] = "three";
+        words[3] = "four";
+        words[4] = "five";
+        words[5] = "six";
+        words[6] = "seven";
+        words[7] = "eight";
+        words[8] = "nine";
+        words[10] = "ten";
     }
 }
