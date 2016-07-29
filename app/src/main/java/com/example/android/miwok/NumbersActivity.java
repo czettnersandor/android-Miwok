6f4417e5b93d8c536d7718f8bf3c1a5752593f62
Sándor Czettner <sandor@czettner.hu>
2016-07-29 21:58:30 +0100

    Dynamically add numbers

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,8 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -26,9 +28,16 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.root_view);
+
+
+
         int index = 0;
-        for (String word : words) {
-            Log.d("NumbersActivity", "Word at index " + index++ + ":" + word);
+        while (index < 4) {
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            rootView.addView(wordView);
+            index++;
         }
     }
 }
